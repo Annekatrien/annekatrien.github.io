4315ee50a556783d0a5c33117a14785f0c5313f1
Annekatrien Debien <annekatriend@gmail.com>
2017-08-31 20:40:41 +0200

    Delete AustinAOI2.js

--- EMSR229_webmap/data/AustinAOI2.js
+++
@@ -1,8 +0,0 @@
-var json_AustinAOI2={
-"type": "FeatureCollection",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-                                                                                
-"features": [
-{ "type": "Feature", "properties": { "Name": "02 AUSTIN - Delineation Map", "Descriptio": "<html xmlns:fo=\"http:\/\/www.w3.org\/1999\/XSL\/Format\" xmlns:msxsl=\"urn:schemas-microsoft-com:xslt\"> <head> <\/head> <body style=\"margin:0px 0px 0px 0px;overflow:auto;background:#FFFFFF;\"> <table style=\"font-family:Arial,Verdana,Times;font-size:12px;text-alig" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -96.154167596999969, 29.21384069000004, 0.0 ], [ -97.714916593999988, 29.21384069000004, 0.0 ], [ -97.706350889999953, 30.635599955000028, 0.0 ], [ -96.127363553999942, 30.61552350900007, 0.0 ], [ -96.154167596999969, 29.21384069000004, 0.0 ] ] ] } }
-]
-}
