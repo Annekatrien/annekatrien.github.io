19b135c78b413078b400720b178afa274c6c4eec
Annekatrien Debien <annekatriend@gmail.com>
2017-08-31 21:21:01 +0200

    Add files via upload
    new URL test

---
+++ mapping/data/HoustonAOI3.js
@@ -0,0 +1,8 @@
+var json_HoustonAOI3={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Name": "01 HOUSTON - Delineation Map", "Descriptio": "<html xmlns:fo=\"http:\/\/www.w3.org\/1999\/XSL\/Format\" xmlns:msxsl=\"urn:schemas-microsoft-com:xslt\"> <head> <\/head> <body style=\"margin:0px 0px 0px 0px;overflow:auto;background:#FFFFFF;\"> <table style=\"font-family:Arial,Verdana,Times;font-size:12px;text-alig" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -93.857016442999964, 28.342259851000051, 0.0 ], [ -96.154881018999959, 28.380214827000032, 0.0 ], [ -96.154167596999969, 29.21384069000004, 0.0 ], [ -96.127363553999942, 30.61552350900007, 0.0 ], [ -93.767532094999979, 30.543421976000051, 0.0 ], [ -93.857016442999964, 28.342259851000051, 0.0 ] ] ] } }
+]
+}
---
+++ mapping/data/MathisAOI1.js
@@ -0,0 +1,8 @@
+var json_MathisAOI1={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Name": "03 MATHIS - Delineation Map", "Descriptio": "<html xmlns:fo=\"http:\/\/www.w3.org\/1999\/XSL\/Format\" xmlns:msxsl=\"urn:schemas-microsoft-com:xslt\"> <head> <\/head> <body style=\"margin:0px 0px 0px 0px;overflow:auto;background:#FFFFFF;\"> <table style=\"font-family:Arial,Verdana,Times;font-size:12px;text-alig" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -98.640157275999968, 27.442764337000028, 0.0 ], [ -98.64193796699999, 28.614495938000061, 0.0 ], [ -97.715054892999945, 28.60769139700005, 0.0 ], [ -97.714916851999988, 27.438804689000051, 0.0 ], [ -98.640157275999968, 27.442764337000028, 0.0 ] ] ] } }
+]
+}
