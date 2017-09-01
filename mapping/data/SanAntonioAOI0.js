fbc89143eb4ecea3f7fcad53baec27ce6f240a88
Annekatrien Debien <annekatriend@gmail.com>
2017-09-01 12:07:06 +0200

    Delete SanAntonioAOI0.js

--- mapping/data/SanAntonioAOI0.js
+++
@@ -1,8 +0,0 @@
-var json_SanAntonioAOI0={
-"type": "FeatureCollection",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-                                                                                
-"features": [
-{ "type": "Feature", "properties": { "Name": "04 SAN ANTONIO - Delineation Map", "Descriptio": "<html xmlns:fo=\"http:\/\/www.w3.org\/1999\/XSL\/Format\" xmlns:msxsl=\"urn:schemas-microsoft-com:xslt\"> <head> <\/head> <body style=\"margin:0px 0px 0px 0px;overflow:auto;background:#FFFFFF;\"> <table style=\"font-family:Arial,Verdana,Times;font-size:12px;text-alig" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -97.705600974999982, 29.823624995000049, 0.0 ], [ -97.714916593999988, 29.21384069000004, 0.0 ], [ -97.715054892999945, 28.60769139700005, 0.0 ], [ -98.896942693999961, 28.615256477000059, 0.0 ], [ -98.903135579999969, 29.828156985000081, 0.0 ], [ -97.705600974999982, 29.823624995000049, 0.0 ] ] ] } }
-]
-}
