44cfc4269576ae1685fca4e1ce501facb20a50c6
Annekatrien Debien <annekatriend@gmail.com>
2017-09-01 12:36:28 +0200

    Add files via upload
    no Houston

---
+++ EMSR229_webmap/data/EMSR22905BEAUMONTORANGE01DELINEATIONv1110000areaofinterest3.js
@@ -0,0 +1,8 @@
+var json_EMSR22905BEAUMONTORANGE01DELINEATIONv1110000areaofinterest3={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "name": "EMSR229_05Beaumont Orange 110k", "act_id": "EMSR229", "subtype": null }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -93.592496785489885, 30.266685112904216 ], [ -93.590298671642174, 29.719165543274492 ], [ -94.356076716660112, 29.714032786066742 ], [ -94.364880411023108, 30.261070708964837 ], [ -93.592496785489885, 30.266685112904216 ] ] ] } }
+]
+}
---
+++ EMSR229_webmap/data/EMSR22906LAKECHARLES01DELINEATIONv162000areaofinterest4.js
@@ -0,0 +1,8 @@
+var json_EMSR22906LAKECHARLES01DELINEATIONv162000areaofinterest4={
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "name": "EMSR229_06Lake Charles_62k", "act_id": "EMSR229", "subtype": null }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -93.031950997636031, 30.345890786784725 ], [ -93.031854535091199, 30.035275600371815 ], [ -93.46745587120806, 30.034624864132983 ], [ -93.468810753352273, 30.344902822752342 ], [ -93.031950997636031, 30.345890786784725 ] ] ] } }
+]
+}
